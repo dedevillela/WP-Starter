ed2fd173bc07ccc89a817310a757dd82fe4bcb8e
André Villela <dede.villela@gmail.com>
2017-05-31 20:28:12 -0300

    Add files via upload

---
+++ child-theme/footer.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * Este arquivo adiciona links customizados ao tema filho do Divi.
+ * Author:   Andre Villela http://dedevillela.com
+ * @example   http://wpsites.net/
+ * @package Divi Parent Theme by Elegant Themes
+*/
+if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
+
+			<footer id="main-footer">
+				<?php get_sidebar( 'footer' ); ?>
+
+
+		<?php
+			if ( has_nav_menu( 'footer-menu' ) ) : ?>
+
+				<div id="et-footer-nav">
+					<div class="container">
+						<?php
+							wp_nav_menu( array(
+								'theme_location' => 'footer-menu',
+								'depth'          => '1',
+								'menu_class'     => 'bottom-nav',
+								'container'      => '',
+								'fallback_cb'    => '',
+							) );
+						?>
+					</div>
+				</div> <!-- #et-footer-nav -->
+
+			<?php endif; ?>
+
+				<div id="footer-bottom">
+					<div class="container clearfix">
+				<?php
+					if ( false !== et_get_option( 'show_footer_social_icons', true ) ) {
+						get_template_part( 'includes/social_icons', 'footer' );
+					}
+				?>
+
+						<p id="footer-info"><?php printf( __( 'Desenvolvido por %1$s', 'Divi' ), '<a href="http://dedevillela.com" title="DedeVillela.com">DedeVillela.com</a>' ); ?></p>
+					</div>	<!-- .container -->
+				</div>
+			</footer> <!-- #main-footer -->
+		</div> <!-- #et-main-area -->
+
+<?php endif; // ! is_page_template( 'page-template-blank.php' ) ?>
+
+	</div> <!-- #page-container -->
+
+	<?php wp_footer(); ?>
+</body>
+</html>
